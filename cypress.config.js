2d2c8e32e3493eb771992180610e853b8a39270d
Poonam <amalepoonam15@gmail.com>
2024-08-14 16:18:44 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  env:{
+    amazonUrl:"https://www.amazon.in",
+    flipcartUrl:"https://www.flipkart.com"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
